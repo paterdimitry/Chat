268b3933d97c6f78df36193412910afcb5e8dc7c
paterdimitry <paterdimitry@gmail.com>
2021-01-12 12:08:18 +0300

    Главное окно пользовательского чата. Урок 4

---
+++ src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        primaryStage.setTitle("Чат");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMinWidth(660);
+        primaryStage.setMinHeight(530);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
